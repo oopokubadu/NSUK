477242e99e0f6f62affebee3ee69bda026145827
lntori <lntori99@gmail.com>
2024-07-05 14:32:08 +0000

    nsano uk'

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,13 +6,13 @@ const Home = () => {
     <div>
       <section className="py-10 bg-[#f9f9f9] sm:py-16 lg:py-24">
         <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-[85rem]">
-          <div className="grid items-center grid-cols-1 gap-y-12 lg:grid-cols-2 lg:gap-x-24">
+          <div className="flex items-center flex-col-reverse  gap-y-12 lg:flex-row lg:gap-x-24">
             <div>
-              <img className="w-full max-w-md mx-auto" src={Globe} alt="" />
+              <img className="w-full mx-auto max-w-xs md:max-w-md" src={Globe} alt="" />
             </div>
 
-            <div className="text-center lg:text-left">
-              <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+            <div className="text-center lg:text-left lg:max-w-2xl">
+              <h2 className="text-4xl font-bold leading-tight text-black  lg:text-5xl">
                 <span className="text-[#F09020]">Nsano UK</span> is your gateway
                 to Africa.
               </h2>
@@ -24,7 +24,6 @@ const Home = () => {
 
               <a
                 href="mailto:info@nsano-uk.com"
-               
                 className="inline-flex items-center rounded-full  justify-center px-8 py-4 font-semibold text-white transition-all duration-200 bg-[#f09020] mt-9"
                 role="button"
               >
