9d13b99e69f868273ee943fcefb83b42e06135dc
lntori <lntori99@gmail.com>
2024-07-05 14:18:33 +0000

    nsano uk'

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,7 @@
 //icons
 export { default as Logo } from "./icons/logo.png";
 export { default as Phone } from "./icons/phone.svg";
+export { default as Logo2 } from "./icons/Logo3.png";
 
 //images
 export { default as Globe } from "./images/Globe.svg";
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Logo2 } from "../assets";
 
 const Footer = () => {
-  return <div className="bg-[#d9d9d9]">
-    <p>Post code: SW16 3PH 443 Streatham road London, England</p>
-  </div>;
+  return (
+    <div className="bg-[#d9d9d9]">
+      <div className="text-center mx-auto text-base py-5">
+       <p >Post code: SW16 3PH</p> 
+        <p >443 Streatham road London,</p>
+        <p >England</p>
+        <p className="py-4 text-sm">Copyright - Nsano UK 2023</p>
+        <img className="mx-auto w-32" src={Logo2} alt=""/>
+      </div>
+
+    </div>
+  );
 };
 
 export default Footer;
